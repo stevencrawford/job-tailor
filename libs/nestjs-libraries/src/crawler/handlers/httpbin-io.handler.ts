af0dfa1d6502c17df2b044b2e5109f8a6f4d7a35
Steven Crawford <steven.crawford.uk@gmail.com>
2024-06-03 15:42:38 +0200

    fix: add missing httpbin handler

---
+++ libs/nestjs-libraries/src/crawler/handlers/httpbin-io.handler.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { CrawlerHandler } from '../crawler-handler.interface';
+import { defaultCrawlerOptions } from '../crawler.defaults';
+import { PlaywrightCrawler, RobotsFile, sleep } from 'crawlee';
+import { InjectRedis } from '@songkeys/nestjs-redis';
+import Redis from 'ioredis';
+
+@Injectable()
+export class HttpBinCrawlerHandler implements CrawlerHandler {
+  _identifier = 'httpbin.io';
+
+  private _robotsFile: RobotsFile;
+
+  constructor(
+    @InjectRedis() private readonly _redis: Redis,
+  ) {
+    this.initializeRobotsFile();
+  }
+
+  async initializeRobotsFile() {
+    this._robotsFile = await RobotsFile.find(`https://${this._identifier}/robots.txt`);
+  }
+
+  supports(url: string): boolean {
+    const domain = new URL(url).hostname.split('.').slice(-2).join('.').toLowerCase();
+    return domain.includes(this._identifier);
+  }
+
+  handle(): PlaywrightCrawler {
+    return new PlaywrightCrawler({
+      ...defaultCrawlerOptions,
+      requestHandler: async ({ page }) => {
+        await page.screenshot({ path: `${this._identifier}_screenshot.png` });
+
+        await sleep(1000);
+      },
+    });
+  }
+}
